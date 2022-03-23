b0df6ca06950b1d63236878223cf4ed09fb83ae9
Yager Guo <yagerguo@126.com>
2022-03-23 15:30:29 +0800

    新增方法

--- src/Stringhw.php
+++ src/Stringhw.php
@@ -23,4 +23,17 @@ class Stringhw
         return $randomString;
     }
 
+    /**
+     * 自动为 url 加上链接
+     * @param string $string
+     * @return string
+     */
+    public static function autolink(string $string): string
+    {
+        $pattern = '/(((http[s]?:\/\/(.+(:.+)?@)?)|(www\.))[a-z0-9](([-a-z0-9]+\.)*\.[a-z]{2,})?\/?[a-z0-9.,_\/~#&=:;%+!?-]+)/is';
+        $string = preg_replace($pattern, ' <a href="$1" class="string-autolink" target="_blank">$1</a>', $string);
+        $string = preg_replace('/href="www/', 'href="http://www', $string);
+        return $string;
+    }
+
 }
