55fae076c6ea1286f99da82aaca96bd6c3f1b76e
Yager Guo <yagerguo@126.com>
2022-03-26 22:39:27 +0800

    不再强制要求 data 为 array

--- src/Returnhw.php
+++ src/Returnhw.php
@@ -5,9 +5,9 @@ namespace Yager\Phphelper;
 
 class Returnhw
 {
-    public int $code;       // 错误码，0 = 成功，其他 = 失败
+    public int $code;           // 错误码，0 = 成功，其他 = 失败
     public ?string $message;    // 错误消息
-    public ?array $data;       // 数据
+    public $data;               // 数据
 
     public function __construct($code = 0, $message = "", $data = null)
     {
@@ -16,7 +16,8 @@ class Returnhw
         $this->data = $data;
     }
 
-    public function getArray(): array{
+    public function getArray(): array
+    {
         return [
             'code' => $this->code,
             'message' => $this->message,
@@ -24,7 +25,8 @@ class Returnhw
         ];
     }
 
-    public function getJsonEncode(): string{
+    public function getJsonEncode(): string
+    {
         return json_encode($this->getArray());
     }
 }
